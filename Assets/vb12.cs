1e8872c84deb3b5545e5c0689a4fbf674ea4c75f
Linkon45 <39150694+Linkon45@users.noreply.github.com>
2019-04-13 12:39:38 +0600

    Info page added

---
+++ Assets/vb12.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class vb12 : MonoBehaviour, Vuforia.IVirtualButtonEventHandler
+{
+
+    private GameObject vButtonObject;
+    // Use this for initialization
+    void Start()
+    {
+        vButtonObject = GameObject.Find("NutVb");
+        vButtonObject.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+
+    }
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("Hello");
+        Application.LoadLevel("another");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+
+    }
+
+
+}
---
+++ Assets/vbScript11.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class vbScript11 : MonoBehaviour, Vuforia.IVirtualButtonEventHandler
+{
+
+    private GameObject vButtonObject;
+    // Use this for initialization
+    void Start()
+    {
+        vButtonObject = GameObject.Find("FamVb");
+        vButtonObject.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+
+    }
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("Hello");
+        Application.LoadLevel("DataMining5");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+
+    }
+
+
+}
